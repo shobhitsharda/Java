188ece9606dfc06350a206b7a6634bf05dea9139
Shobhit Sharda <shobhitsharda@gmail.com>
2018-12-06 18:07:33 +0100

    - added tree

---
+++ src/tree/BinarySearchTreeUtil.java
@@ -0,0 +1,16 @@
+package tree;
+
+public class BinarySearchTreeUtil {
+
+	public boolean areTreeSame(final Node rootNode1, final Node rootNode2) {
+		if (rootNode1 == null && rootNode2 == null) {
+			return true;
+		}
+
+		if (rootNode1 == null || rootNode2 == null) {
+			return false;
+		}
+
+		return rootNode1.data == rootNode2.data && areTreeSame(rootNode1.left, rootNode2.left) && areTreeSame(rootNode1.right, rootNode2.right);
+	}
+}
---
+++ src/tree/Node.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class Node {
+	int data;
+	Node left;
+	Node right;
+
+	Node(final int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
